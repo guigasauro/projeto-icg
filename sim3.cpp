23c963fec8103bb452206a2aca2faa53c5dfd271
guilherme.ns <guilherme.silva.ext@synchro.com.br>
2025-04-30 01:34:19 -0300

    teste

--- sim3.cpp
+++ sim3.cpp
@@ -368,6 +368,8 @@ int main() {
     }
     
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
     GLuint backgroundTexture;
